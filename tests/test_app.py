9b00ab77b340a10d9878e6803a51aebd4528bb5c
gwenbleidd@32 <premiumforges32@gmail.com>
2024-08-02 00:14:42 -0700

    fix-reporting

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,9 +12,13 @@ def test_index(client):
     assert rv.status_code == 200 # Tests if HTTP Response is OK by being a 200 Response
     print(rv.data)  # Debug print statement to inspect the response content
     #assert b'APPLICATION-A' in rv.data or b'APPLICATION-B' in rv.data  # Updated to match the application content
-    assert b'Counter Value' in rv.data
+    assert b'Counter Value' in rv.data 
 
 def test_increment(client):
     rv = client.post('/increment')
     assert rv.status_code == 200
-    assert b'counter' in rv.data
+    assert b'counter' in rv.data 
+
+
+
+# Dynamic Functional Testing is invoked to Confirm http connection is valid and specified assertions are met and located in the file.
