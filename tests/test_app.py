d81be14865f63294bc5901191b1954a76566ba78
gwenbleidd@32 <premiumforges32@gmail.com>
2024-08-01 23:42:16 -0700

    test-functional

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,17 @@
 import pytest
 from app import app
-
+#Functional Testing of my application through a sonar cloud quality gate. 
 @pytest.fixture
-def client():
+def client(): #Set's up the client for testing
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
 def test_index(client):
     rv = client.get('/')
-    assert rv.status_code == 200
+    assert rv.status_code == 200 # Tests if HTTP Response is OK by being a 200 Response
     print(rv.data)  # Debug print statement to inspect the response content
-    assert b'APPLICATION-A' in rv.data or b'APPLICATION-B' in rv.data  # Updated to match the application content
+    #assert b'APPLICATION-A' in rv.data or b'APPLICATION-B' in rv.data  # Updated to match the application content
     assert b'Counter Value' in rv.data
 
 def test_increment(client):
