64f2dfe49078730982867ea04cc60b28394060d3
gwenbleidd@32 <premiumforges32@gmail.com>
2024-08-25 15:18:44 -0700

    updat-app

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def metrics():
     return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
